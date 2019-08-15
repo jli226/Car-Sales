905b8ef7cdf8b658ec5a63ca2ddc1e62b25983f7
Jenny Li <jli226@fordham.edu>
2019-08-15 08:29:31 -0700

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { removeFeature } from "./../actions";
 
-const AddedFeature = props => {
+const AddedFeature = ({ feature, removeFeature }) => {
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button onClick={() => removeFeature(feature)} className="button">
+        X
+      </button>
+      {feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(
+  null,
+  { removeFeature }
+)(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import { addFeature } from "./../actions";
+import { connect } from "react-redux";
 
-const AdditionalFeature = props => {
+const AdditionalFeature = ({ addFeature, feature }) => {
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button onClick={() => addFeature(feature)} className="button">
+        Add
+      </button>
+      {feature.name} (+{feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(
+  null,
+  { addFeature }
+)(AdditionalFeature);
